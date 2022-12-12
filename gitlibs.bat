f947fd8069c489f190c828dd36de372c376e7d2e
jacobvc <vince@yourdevelopmentteam.com>
2022-12-12 14:00:07 -0600

    Delete gitlibs.bat

--- gitlibs.bat
+++
@@ -1,4 +0,0 @@
-git clone -b protocol-processor-destructor https://github.com/jacobvc/ViscaLibrary ViscaLibrary-main
-
-git clone https://github.com/ninjasource/Ninja.WebSockets.git Ninja.WebSockets
-pause
